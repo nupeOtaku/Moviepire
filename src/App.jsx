b70451e38fadd29622b68d2b2265c416044c64d2
NupeCoder <NupeCoder@gmail.com>
2023-12-16 15:10:17 +0100

    installed Eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-const App = () => {
-  return <div></div>;
-};
+// eslint-disable-next-line max-len
+// eslint-disable-next-line react/function-component-definition, react/react-in-jsx-scope, react/self-closing-comp
+const App = () => <div></div>;
 
 export default App;
